369075b40bf60246e8e44ee9a210f36640ed76fe
Tahiry2507 <utouch25@gmail.com>
2014-01-08 14:25:28 +0300

    Update ruby-project-4.rb

--- ruby-project-4.rb
+++ ruby-project-4.rb
@@ -1,3 +1,5 @@
+# Step 2
+# a) Read on Ruby classes
 # b) Create a simple Ruby class called "Book"
 
 	class Book
@@ -23,6 +25,13 @@
 	# book1's author_name doesn't change to "Robinson Crusoe" Ruby is using what we call "Syntactic sugar"
 
 # 3- Basic Ruby Stuff
+	# a)Create a constant variable. What are constants? Why do we use them? How are they different than other ruby variables?
+
+		# TAHIRY = "Aina Tahiry"
+		
+		# A constant is a variable with a fixed value and its value remains constant for 
+		# the duration of the program. Constant variable names begin with a capital letter.
+
 	# b) implement a variety of loops
 
 	number = 0
@@ -43,7 +52,6 @@
 	for number in 0..10
 		puts number
 	end
-	puts "Loops using For is over\n\n"
 
 	# c) Do some math
 
@@ -57,6 +65,18 @@
 	d = x / 16
 
 	puts "d is equal to #{d}"
+	# d) What is a boolean?
+	
+		# a boolean is like a variable that can only have one of two possible values, true or false.
+	
+	# e) if a is a boolean (set to true), what would !a return?
+	
+		# if a is a boolean (true) !a would return nil
+	
+	# f) How are these two statements different? a == "hi" and a = "hi"
+	
+		#  a == 'hi' means a is equal to the string 'hi' ( usually used on comparison or loops)
+		#  a = 'hi' the variable a is set to the value 'hi'
 
 	# g) Provide an example of using "&&" and "||"
 
@@ -66,5 +86,24 @@
 
 	if a < b || b > 40
 		puts "b is greater than a"
+	end
+	
+	# h) Implement a case statement
+	
+	print "What is your name?"
+	name = gets.chomp
+	case name
+	when "Steve", "Jacob", "Tahiry"
+		puts "Welcome, #{name}"
+	else
+		puts "Sorry, I don't know you! :("
+	end
+	
+	# i) Create a loop that prints 1 through 20
+	
+	w = 1
+	until w > 20 do
+		puts w
+		w += 1
 	end
 	
